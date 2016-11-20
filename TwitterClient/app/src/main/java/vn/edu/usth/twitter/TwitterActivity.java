4c50da7153cebaedca5eabbbf606f180f42a733c
Tung Lam <ngstnglm@gmail.com>
2016-11-20 23:24:57 +0700

    add toolbar + menu

--- TwitterClient/app/src/main/java/vn/edu/usth/twitter/TwitterActivity.java
+++ TwitterClient/app/src/main/java/vn/edu/usth/twitter/TwitterActivity.java
@@ -8,6 +8,9 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import vn.edu.usth.twitter.Fragment.Home;
 
@@ -26,6 +29,9 @@ public class TwitterActivity extends AppCompatActivity {
 
         pager.setOffscreenPageLimit(3);
         pager.setAdapter(adapter);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
     }
 
     public class HomeFragmentPagerAdapter extends FragmentPagerAdapter {
@@ -53,6 +59,23 @@ public class TwitterActivity extends AppCompatActivity {
         public CharSequence getPageTitle(int page) {
 // returns a tab title corresponding to the specified page
             return titles[page];
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_twitter, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                // do something when search is pressed here
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
