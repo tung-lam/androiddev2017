29951f0b942e2ccbb857bbb6654fa42925dca6ee
Tung Lam <ngstnglm@gmail.com>
2016-11-20 17:10:37 +0700

    make twitter activity as launcher

---
+++ TwitterClient/app/src/main/java/vn/edu/usth/twitter/TwitterActivity.java
@@ -0,0 +1,13 @@
+package vn.edu.usth.twitter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TwitterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_twitter);
+    }
+}
