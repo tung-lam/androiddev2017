9956cc0635900785feec73b7912d65818b3405be
Tung Lam <ngstnglm@gmail.com>
2016-11-30 20:19:56 +0700

    press button to go to the main UI

--- TwitterClient/app/src/main/java/vn/edu/usth/twitter/LoginActivity.java
+++ TwitterClient/app/src/main/java/vn/edu/usth/twitter/LoginActivity.java
@@ -121,13 +121,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         loginButton.setCallback(new Callback<TwitterSession>() {
             @Override
             public void success(Result<TwitterSession> result) {
-                // The TwitterSession is also available through:
-                // Twitter.getInstance().core.getSessionManager().getActiveSession()
-                TwitterSession session = result.data;
-                // TODO: Remove toast and use the TwitterSession's userID
-                // with your app's user model
-                String msg = "@" + session.getUserName() + " logged in! (#" + session.getUserId() + ")";
-                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(LoginActivity.this, TwitterActivity.class);
+                startActivity(intent);
             }
             @Override
             public void failure(TwitterException exception) {
