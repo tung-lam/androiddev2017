0f8606eb802e042cbe48721a74d85cf96d3cb71e
Tung Lam <ngstnglm@gmail.com>
2016-12-04 00:38:24 +0700

    The Mentions Timeline now appears!

--- TwitterClient/app/src/main/java/vn/edu/usth/twitter/Fragment/Notification.java
+++ TwitterClient/app/src/main/java/vn/edu/usth/twitter/Fragment/Notification.java
@@ -11,10 +11,22 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.twitter.sdk.android.Twitter;
+
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.models.Tweet;
+import com.twitter.sdk.android.tweetui.FixedTweetTimeline;
 import com.twitter.sdk.android.tweetui.SearchTimeline;
+import com.twitter.sdk.android.tweetui.Timeline;
 import com.twitter.sdk.android.tweetui.TweetTimelineListAdapter;
+import com.twitter.sdk.android.tweetui.TwitterListTimeline;
 import com.twitter.sdk.android.tweetui.UserTimeline;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import vn.edu.usth.twitter.R;
 
@@ -27,15 +39,34 @@ public class Notification extends ListFragment {
         super.onCreate(savedInstanceState);
         TwitterSession session = Twitter.getSessionManager().getActiveSession();
 
-        UserTimeline userTimeline = new UserTimeline.Builder().userId(session.getUserId()).build();
-        final SearchTimeline searchTimeline = new SearchTimeline.Builder()
-                .query("from:khanhtrannnn")
-                .build();
-        final TweetTimelineListAdapter adapter = new TweetTimelineListAdapter.Builder(getActivity())
-                .setTimeline(searchTimeline)
-                .build();
-        setListAdapter(adapter);
+        final List<Tweet> tweets = new ArrayList<>();
+//        android.util.Log.d("Number of tweets: ", String.valueOf(tweets.size()));
+        TwitterCore.getInstance().getApiClient(session).getStatusesService()
+                .mentionsTimeline(null, null, null, null, null, null).enqueue(
+                new Callback<List<Tweet>>() {
+                    @Override
+                    public void success(Result<List<Tweet>> result) {
+                        for (Tweet t : result.data) {
+                            tweets.add(t);
+                            android.util.Log.d("twittercommunity", "tweet is " + t.text);
+                        }
+                        final FixedTweetTimeline fixedTweetTimeline = new FixedTweetTimeline.Builder()
+                                .setTweets(result.data)
+                                .build();
 
+                        final TweetTimelineListAdapter adapter = new TweetTimelineListAdapter.Builder(getActivity())
+                                .setTimeline(fixedTweetTimeline)
+                                .build();
+                        setListAdapter(adapter);
+                    }
+
+                    @Override
+                    public void failure(TwitterException exception) {
+                        android.util.Log.d("twittercommunity", "exception " + exception);
+                    }
+                }
+        );
+        android.util.Log.d("Number of tweets: ", String.valueOf(tweets.size()));
     }
 
 
